d512494f533b4d0609528ce26c3faa53a1df8424
Cheng Guo <cheng.guo@gmail.com>
2013-07-11 13:56:40 +0000

    Add real grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://gentle-retreat-7797.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +57,70 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtml = function(html, checksfile){
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var saveToFile = function(outJson){
+
+    console.log(outJson);
+
+    if(outJson != null){
+	fs.writeFile("checkUrl", outJson, function(err){
+		if(err){
+		    console.log(err);
+		} else {
+		    console.log("File saved as \"checkurl\"");
+		}
+	    });
+    } else {
+	console.log("Error cannot output Json");
+    }
+};
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to any url', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var args = process.argv;
+
+    for(var obj in args)
+    {
+	if(args[obj].localeCompare("-f") == 0)
+	{
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    saveToFile(outJson);
+	    break;
+	}
+	else if(args[obj].localeCompare("-u") == 0)
+	{
+	    rest.get(program.url).on('complete', function(result){
+		if(result instanceof Error) {
+		    console.log("Error loading " + program.url);
+		} else {
+		    var checkUrl = checkHtml(result, program.checks);
+		    var outJson = JSON.stringify(checkUrl, null, 4);
+		    saveToFile(outJson);
+		}
+	    });
+	    break;
+	}
+    }    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
